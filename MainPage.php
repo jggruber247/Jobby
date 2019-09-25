7d58b5dd556153988d4ade9d263c0a22575cf20b
jggruber247 <jggruber247@gmail.com>
2019-09-25 13:31:04 -0400

    First commit

---
+++ MainPage.php
@@ -0,0 +1,12 @@
+<!--AUTHOR: James Garrett Gruber-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jobby: The Job Search Organizer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="Mainpage.css"> 
+</head>
+<body>
+
+</body>
+</html>
