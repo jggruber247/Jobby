953ce2f2395fc406294628234b9803127ce4fadf
jggruber247 <jggruber247@gmail.com>
2019-09-25 14:25:23 -0400

    Creating Login page

---
+++ Login.php
@@ -0,0 +1,12 @@
+<!--AUTHOR: James Garrett Gruber-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login to Jobby</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="Login.css"> 
+</head>
+<body>
+
+</body>
+</html>
