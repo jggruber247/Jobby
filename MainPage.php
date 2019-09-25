b3242e39cdac5a61c240e59dfea4e48578b5c654
jggruber247 <jggruber247@gmail.com>
2019-09-25 14:22:40 -0400

    Progress

--- MainPage.php
+++ MainPage.php
@@ -4,9 +4,11 @@
 <head>
 	<title>Jobby: The Job Search Organizer</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="Mainpage.css"> 
+	<link rel="stylesheet" href="MainPage.css"> 
 </head>
 <body>
+<h1>Jobby: Organizing your job search since 2019!</h1>
+
 
 </body>
 </html>
